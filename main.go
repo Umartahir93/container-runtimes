ae8a9f386f0e267f2404c0f825ba02de2eec6c31
umar-tahir <umartahir@live.com>
2026-01-25 17:43:59 +0100

    feat: add networking capability to the rootless container

--- main.go
+++ main.go
@@ -2,15 +2,17 @@ package main
 
 import (
 	"fmt"
+	"net"
 	"os"
 	"os/exec"
 	"path/filepath"
 	"syscall"
+	"time"
 )
 
 func main() {
 	if len(os.Args) < 2 {
-		panic("expected parent or child")
+		panic("Usage: go run main.go parent <command> <args>")
 	}
 
 	switch os.Args[1] {
@@ -21,157 +23,118 @@ func main() {
 	default:
 		panic("unknown command")
 	}
-
 }
 
-// parent function invoked main program which sets up the namespace
 func parent() {
 	cmd := exec.Command("/proc/self/exe", append([]string{"child"}, os.Args[2:]...)...)
-	// The statements below refer to the input, output and error streams of the process created (cmd)
 	cmd.Stdin = os.Stdin
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
-	//setting an environment variable
 	cmd.Env = []string{"name=shashank"}
 
-	// The | operator is bitwise OR, combining multiple flags into a single number which kernel understands to
-	// create process with all new namespaces
 	cmd.SysProcAttr = &syscall.SysProcAttr{
 		Cloneflags: syscall.CLONE_NEWUTS |
 			syscall.CLONE_NEWPID |
 			syscall.CLONE_NEWNS |
 			syscall.CLONE_NEWIPC |
-			syscall.CLONE_NEWUSER,
+			syscall.CLONE_NEWUSER |
+			syscall.CLONE_NEWNET,
 		UidMappings: []syscall.SysProcIDMap{
-			{
-				ContainerID: 0,
-				HostID:      os.Getuid(),
-				Size:        1,
-			},
+			{ContainerID: 0, HostID: os.Getuid(), Size: 1},
 		},
 		GidMappings: []syscall.SysProcIDMap{
-			{
-				ContainerID: 0,
-				HostID:      os.Getgid(),
-				Size:        1,
-			},
+			{ContainerID: 0, HostID: os.Getgid(), Size: 1},
 		},
 	}
 
-	err := cmd.Run()
-	if err != nil {
-		fmt.Printf("E%s\n", err)
+	if err := cmd.Start(); err != nil {
+		fmt.Printf("Error starting child: %v\n", err)
+		os.Exit(1)
+	}
+
+	fmt.Printf("Parent: Child PID is %d. Setting up slirp4netns...\n", cmd.Process.Pid)
+	go setupNetworking(cmd.Process.Pid)
+
+	if err := cmd.Wait(); err != nil {
+		fmt.Printf("Child exited with error: %v\n", err)
 	}
 }
 
-// this is the child process w copy of the parent program
+func setupNetworking(pid int) {
+	args := []string{
+		"--configure",
+		"--mtu=1500",
+		fmt.Sprintf("%d", pid),
+		"tap0",
+	}
+
+	netCmd := exec.Command("slirp4netns", args...)
+	if err := netCmd.Run(); err != nil {
+		fmt.Printf("slirp4netns error: %v\n", err)
+	}
+}
+
 func child() {
+	syscall.Sethostname([]byte("myhost"))
+
+	rootfs, _ := filepath.Abs("./rootfs")
+	if err := setupRoot(rootfs); err != nil {
+		fmt.Printf("Root setup error: %v\n", err)
+		os.Exit(1)
+	}
+
+	if err := mountProc(); err != nil {
+		fmt.Printf("Mount proc error: %v\n", err)
+		os.Exit(1)
+	}
+
+	exec.Command("ip", "link", "set", "lo", "up").Run()
+
 	cmd := exec.Command(os.Args[2], os.Args[3:]...)
 	cmd.Stdin = os.Stdin
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
+	cmd.Env = []string{"PS1=\\u@\\h:\\w# ", "PATH=/bin:/usr/bin:/sbin"}
 
-	// This line tells the new shell how to look:
-	// \u = username, \h = hostname, \w = current directory
-	cmd.Env = []string{"PS1=\\u@\\h:\\w# "}
-
-	// the command below sets the h to myhost. Idea here is to show use of UTS namespace.
-	err := syscall.Sethostname([]byte("myhost"))
+	err := waitForNetwork()
 	if err != nil {
-		fmt.Printf("E%s\n", err)
+		fmt.Printf("Network wait error: %v\n", err)
 	}
 
-	rootfs, err := filepath.Abs("./rootfs")
-	if err != nil {
-		fmt.Printf("E%s\n", err)
-	}
-
-	if err = setupRoot(rootfs); err != nil {
-		fmt.Printf("pivot-root E%s\n", err)
+	if err := cmd.Run(); err != nil {
+		fmt.Printf("Command execution error: %v\n", err)
 		os.Exit(1)
 	}
-
-	if err = mountProc(); err != nil {
-		fmt.Printf("mount Proc%s\n", err)
-		os.Exit(1)
-	}
-
-	err = cmd.Run()
-	if err != nil {
-		fmt.Printf("E%s\n", err)
-	}
-
 }
 
 func setupRoot(newRoot string) error {
 	if err := syscall.Chroot(newRoot); err != nil {
 		return err
 	}
-	if err := os.Chdir("/"); err != nil {
-		return err
-	}
-	return nil
+	return os.Chdir("/")
 }
 
 func mountProc() error {
-	source := "proc"
 	target := "/proc"
-
-	if err := os.MkdirAll(target, 0755); err != nil {
-		return err
-	}
-
-	if err := syscall.Mount(source, target, "proc", 0, ""); err != nil {
-		return err
-	}
-	return nil
+	os.MkdirAll(target, 0755)
+	return syscall.Mount("proc", target, "proc", 0, "")
 }
 
-/*
-	pivotRoot is commented out because it requires CAP_SYS_ADMIN, which is
-	not available in rootless user namespaces. Using pivot_root() would fail
-	with "operation not permitted" .
+func waitForNetwork() error {
+	maxWait := 15 * time.Second
+	timeStarted := time.Now()
 
-	We use chroot() instead to allow rootless containers to run safely.
-	pivot_root() should be used only in rootful containers (with CAP_SYS_ADMIN)
-	because it provides stronger filesystem isolation than chroot().
-*/ /*func pivotRoot(newRoot string) error {
-	// Ensure new root is a mount point
-	if err := syscall.Mount(
-		newRoot,
-		newRoot,
-		"",
-		syscall.MS_BIND|syscall.MS_REC,
-		"",
-	); err != nil {
-		return err
+	for {
+		interfaces, err := net.Interfaces()
+		if err != nil {
+			return err
+		}
+		if len(interfaces) > 1 {
+			return nil
+		}
+		if time.Since(timeStarted) > maxWait {
+			return fmt.Errorf("timeout waiting for network interface to become available")
+		}
+		time.Sleep(1 * time.Second)
 	}
-
-	putOld := filepath.Join(newRoot, ".pivot_root")
-
-	// create directory to put old root
-	if err := os.Mkdir(putOld, 0777); err != nil {
-		return err
-	}
-
-	// perform pivot root
-	if err := syscall.PivotRoot(newRoot, putOld); err != nil {
-		return err
-	}
-
-	// change working directory to new root
-	if err := os.Chdir("/"); err != nil {
-		return err
-	}
-
-	putOld = "/.pivot_root"
-	if err := syscall.Unmount(putOld, syscall.MNT_DETACH); err != nil {
-		return err
-	}
-
-	if err := os.Remove(putOld); err != nil {
-		return err
-	}
-
-	return nil
-}*/
+}
